249d505d5b48b1bc8c92dd495e1922d4f2b0b612
unknown <kamolov1575@gmail.com>
2024-04-11 03:48:33 +0500

    first commit

--- db.js
+++ db.js
@@ -1,12 +1,19 @@
-const {Sequelize} = require('sequelize')
+const { Sequelize } = require('sequelize');
 
 module.exports = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.POSTGRES_DATABASE, // Use the database name from the environment variable
+    process.env.POSTGRES_USER,     // Use the database user from the environment variable
+    process.env.POSTGRES_PASSWORD, // Use the database password from the environment variable
     {
         dialect: 'postgres',
-        host: process.env.DB_HOST,
-        post: process.env.DB_PORT,
+        host: process.env.POSTGRES_HOST, // Use the database host from the environment variable
+        port: process.env.DB_PORT,       // Use the database port from the environment variable
+        ssl: true, // Ensure SSL is enabled
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false // Avoid rejection of unauthorized connections
+            }
+        }
     }
-)
+);
