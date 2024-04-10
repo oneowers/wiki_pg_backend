3ec23251c668c82c75cce0ac390c45e4e6980f8a
unknown <kamolov1575@gmail.com>
2024-04-11 04:35:41 +0500

    first commit

--- db.js
+++ db.js
@@ -5,6 +5,7 @@ module.exports = new Sequelize(
     process.env.POSTGRES_USER,     // Use the database user from the environment variable
     process.env.POSTGRES_PASSWORD, // Use the database password from the environment variable
     {
+        dialectModule: require('pg'),
         dialect: 'postgres',
         host: process.env.POSTGRES_HOST, // Use the database host from the environment variable
         port: process.env.DB_PORT,       // Use the database port from the environment variable
