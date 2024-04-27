5bdcf83e1a7f93a80560ed5e90f68e2015468510
Rokki Khazratov <rokkikhazratov@gmail.com>
2024-04-28 01:23:36 +0500

    fornitures

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -1,6 +1,9 @@
 const { User, Brand } = require("../models/models");
 const jwt = require("jsonwebtoken");
 
+
+
+
 class BrandController {
   async create(req, res) {
     const {
@@ -44,4 +47,39 @@ class BrandController {
   }
 }
 
+
+// class FornitureController {
+//   async create(req, res) {
+//     const {
+//       name,
+//       img,
+//       unit_of_measure,
+//       price,
+//     } = req.body;
+
+//     // Получите идентификатор пользователя из токена
+//     const token = req.headers.authorization.split(" ")[1];
+//     const decoded = jwt.verify(token, process.env.SECRET_KEY);
+//     const userId = decoded.id;
+
+//     // Создайте бренд и сохраните его id для пользователя
+//     const forniture = await Forniture.create({
+//       name,
+//       img,
+//       unit_of_measure,
+//       price,
+//     });
+//     const user = await User.findByPk(userId);
+//     await user.update({ FornitureId: forniture.id });
+
+//     return res.json(forniture);
+//   }
+
+//   async getAll(req, res) {
+//     const fornitures = await Forniture.findAll();
+//     return res.json(fornitures);
+//   }
+// }
+
+// module.exports = new FornitureController();
 module.exports = new BrandController();
