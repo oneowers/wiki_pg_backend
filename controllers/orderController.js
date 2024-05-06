9219ab0f1070a9db51f4a5d7d8e5c05897f2e478
Muhammad Kamolov <muhammadkamolov@MacBook-Air-Muhammad.local>
2024-05-06 23:32:52 +0500

    models fix

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -13,13 +13,27 @@ class OrderController {
 
   async create(req, res) {
     try {
-      const { scene_size_x, scene_size_y, furniture_list, user_id, brand_id } = req.body;
-      const order = await Order.create({ scene_size_x, scene_size_y, furniture_list: parsedFurnitureList, user_id, brand_id });
-      return res.json(order);
+        const { scene_size_x, scene_size_y, furniture_list, user_id, brand_id } = req.body;
+        
+        // Проверяем, что furniture_list является строкой
+        if (typeof furniture_list !== 'string') {
+            throw new Error('furniture_list should be a string');
+        }
+        
+        // Парсим JSON строку
+        const parsedFurnitureList = JSON.parse(furniture_list);
+
+        // Создаем заказ
+        const order = await Order.create({ scene_size_x, scene_size_y, furniture_list: parsedFurnitureList, user_id, brand_id });
+
+        // Возвращаем созданный заказ
+        return res.json(order);
     } catch (error) {
-      return res.status(500).json({ error: error.message });
+        // Если произошла ошибка, возвращаем статус 500 и сообщение об ошибке
+        return res.status(500).json({ error: error.message });
     }
-  }
+}
+
 }
 
 module.exports = new OrderController();
