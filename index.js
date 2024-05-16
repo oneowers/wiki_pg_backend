848abe264e7acbf7ff6792c60e466218522d80a7
Muhammad Kamolov <muhammadkamolov@MacBook-Air-Muhammad.local>
2024-05-16 19:05:41 +0500

    models fix

--- index.js
+++ index.js
@@ -1,32 +1,31 @@
-require('dotenv').config()
-const sequelize = require('./db')
-const express = require('express')
-const cors = require('cors')
-const fileUpload = require('express-fileupload')
-const router = require('./routes/index')
-const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const dotenv = require('dotenv');
+dotenv.config();
+const sequelize = require('./db');
+const express = require('express');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
 const path = require('path');
+const favicon = require('serve-favicon'); // Import serve-favicon module
 
-// Добавляем путь к favicon
-const faviconPath = path.resolve(__dirname, 'globalcyber.ico'); // Замените 'path_to_your_favicon.ico' на путь к вашему файлу favicon
+const PORT = process.env.PORT || 5000;
 
-// Добавляем middleware для обработки favicon
-app.use(express.favicon(faviconPath));
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
+app.use('/api', router);
 
+// Path to your favicon
+const faviconPath = path.join(__dirname, 'globalcyber.ico'); // Replace 'path_to_your_favicon.ico' with the path to your favicon
 
-
-const PORT = process.env.PORT || 5000
-
-const app = express()
-app.use(cors())
-app.use(express.json())
-app.use(express.static(path.resolve(__dirname, 'static')))
-app.use(fileUpload({}))
-app.use('/api', router)
-
+// Middleware to serve favicon
+app.use(favicon(faviconPath));
 
 // End!!
-app.use(errorHandler)
+app.use(errorHandler);
 
 app.get('/', (req, res) => {
     try {
@@ -37,16 +36,14 @@ app.get('/', (req, res) => {
     }
 });
 
-
-
 const start = async () => {
-    try{
-        await sequelize.authenticate()
-        await sequelize.sync()
-        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
-    }catch(e){
-        console.log(e)
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+    } catch (e) {
+        console.log(e);
     }
-}
+};
 
-start()
+start();
