4e140592010fb16012285cc0a1ac3bf2f20fcd31
Muhammad Kamolov <muhammadkamolov@MacBook-Air-Muhammad.local>
2024-05-21 02:21:29 +0500

    models fix

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,14 @@ const brandRouter = require('./brandRouter')
 const typeRouter = require('./typeRouter')
 const furnitureRouter = require('./furnitureRouter')
 const orderRouter = require('./orderRouter')
+const participantRouter = require('./participantRouter')
 
 router.use('/user', userRouter)
 router.use('/type', typeRouter)
 router.use('/brand', brandRouter)
 router.use('/device', deviceRouter)
 router.use('/furniture', furnitureRouter)
+router.use('/participant', participantRouter)
 router.use('/order', orderRouter)
 
 module.exports = router
